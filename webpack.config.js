8cdcb38cc374db75c10a7a9105af362a0e1fa3c1
Patrick O'Regan <patrick.oregan@pricerunner.com>
2019-03-10 19:27:15 +0100

    add webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,24 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: ["babel-loader", "eslint-loader"],
+        loader: ['babel-loader', 'eslint-loader'],
       },
     ],
+  },
+  devServer: {
+    publicPath: 'http://localhost:3000/dist',
+    port: 3000,
+    contentBase: path.resolve(__dirname, 'public'),
   },
 };
